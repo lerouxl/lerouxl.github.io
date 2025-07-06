37402917836f6d443d0969cef0dd6274fcdaee12
leopold <noreply@noreply.com>
2025-07-06 12:35:22 +0200

    Fix links

--- js/script.js
+++ js/script.js
@@ -3,3 +3,15 @@ if (document.getElementById('my-work-link')) {
     document.getElementById('my-work-section').scrollIntoView({behavior: "smooth"})
   })
 }
+
+if (document.getElementById('my-projects-link')) {
+  document.getElementById('my-projects-link').addEventListener('click', () => {
+    document.getElementById('my-projects-section').scrollIntoView({behavior: "smooth"})
+  })
+}
+
+if (document.getElementById('research-papers-link')) {
+  document.getElementById('research-papers-link').addEventListener('click', () => {
+    document.getElementById('research-papers-section').scrollIntoView({behavior: "smooth"})
+  })
+}
